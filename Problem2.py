9832bbad27eb7f8b944cce0456bd19c076eb9df6
MathGeek10 <33035589+MathGeek10@users.noreply.github.com>
2017-11-03 23:39:36 -0700

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,1 +1,14 @@
-
+def evenFibSum(limit):
+  a, b = 0, 1
+  evenSum = 0
+  
+  while(b < limit):
+    temp = a
+    a = b
+    if a % 2 == 0:
+      evenSum += a
+    b = temp + b
+  
+  return evenSum
+  
+print(evenFibSum(4000000))
